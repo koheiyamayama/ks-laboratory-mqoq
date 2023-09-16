96cfa49b0b5930da3ac96edd0884c0c1980d941b
koheiyamayama <kohei.y.23@gmail.com>
2023-09-17 02:09:00 +0900

    QUICサーバーを起動する

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/koheiyamayama/ks-laboratory-mqoq
+
+go 1.21
+
+require (
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20230912144702-c363fe2c2ed8 // indirect
+	github.com/onsi/ginkgo/v2 v2.12.0 // indirect
+	github.com/quic-go/qtls-go1-20 v0.3.4 // indirect
+	github.com/quic-go/quic-go v0.38.1 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/quic-go/quic-go"
+	"golang.org/x/exp/slog"
+)
+
+func main() {
+	ctx := context.Background()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+	udpConn, err := net.ListenUDP("udp4", &net.UDPAddr{Port: 1234})
+	if err != nil {
+		logger.ErrorContext(ctx, err.Error())
+		os.Exit(1)
+	}
+	tr := quic.Transport{
+		Conn: udpConn,
+	}
+	ln, err := tr.Listen(&tls.Config{}, &quic.Config{})
+	if err != nil {
+		logger.ErrorContext(ctx, err.Error())
+		os.Exit(1)
+	}
+
+	logger.InfoContext(ctx, "start ks-laboratory-mqoq on 1234 port")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			conn, err := ln.Accept(ctx)
+			if err != nil {
+				logger.ErrorContext(ctx, err.Error())
+				os.Exit(1)
+			}
+			err = handleConnection(conn)
+			if err != nil {
+				logger.ErrorContext(ctx, err.Error())
+				os.Exit(1)
+			}
+		}
+	}()
+	wg.Wait()
+}
+
+func handleConnection(conn quic.Connection) error {
+	msg, err := conn.ReceiveMessage(conn.Context())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("message: ", string(msg))
+
+	return nil
+}
